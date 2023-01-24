1c0d60660ab36bd4bcff58789c7b0a9635e3653f
JunseongKim0105 <juns.kim0105@gmail.com>
2023-01-24 16:59:49 -0500

    modified

--- express_server.js
+++ express_server.js
@@ -2,11 +2,33 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const randomIDGenerate = (numberOfChar) => {
+  const template =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const results = [];
+  for (let i = 0; i < numberOfChar; i++) {
+    const randomIndex = Math.round(Math.random() * 62);
+    results.push(template[randomIndex]);
+  }
+  return results.join('');
+};
+
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.get('/', (req, res) => {
+  res.send(`Hello World!`);
+});
+
 app.get('/urls/:id', (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -20,9 +42,17 @@ app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
-app.get('/hello', (req, res) => {
-  const templateVars = { greeting: 'Hello World!' };
-  res.render('hello_world', templateVars);
+app.get('/u/:id', (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  res.redirect(longURL);
+});
+
+app.post('/urls', (req, res) => {
+  const longURL = req.body.longURL;
+  const id = randomIDGenerate(6);
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls${id}`);
 });
 
 app.get('/hello', (req, res) => {
