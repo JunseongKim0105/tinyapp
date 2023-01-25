5a1b5c3ca9857ace1a708addaa6d108ce354ef7a
JunseongKim0105 <juns.kim0105@gmail.com>
2023-01-24 22:44:17 -0500

    delete function

--- express_server.js
+++ express_server.js
@@ -20,13 +20,28 @@ const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
+app.get('/set', (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
 
+app.get('/fetch', (req, res) => {
+  res.send(`a = ${a}`);
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
 app.get('/', (req, res) => {
   res.send(`Hello World!`);
+});
+app.get('/hello', (req, res) => {
+  const templateVars = { greeting: 'Hello World!' };
+  res.render('hello_world', templateVars);
 });
 
 app.get('/urls/:id', (req, res) => {
@@ -52,9 +67,13 @@ app.post('/urls', (req, res) => {
   const longURL = req.body.longURL;
   const id = randomIDGenerate(6);
   urlDatabase[id] = longURL;
-  res.redirect(`/urls${id}`);
+  res.redirect(`/urls/${id}`);
 });
-
+app.post('/urls/:id/delete', (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect('/urls');
+});
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
